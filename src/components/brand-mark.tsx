17ed0444f7939e2ef66070a29cbe457d3b61678d
Oyinade003 <opedamola003@gmail.com>
2026-09-24 07:51:34 +0100

    Updated logo png

--- src/components/brand-mark.tsx
+++ src/components/brand-mark.tsx
@@ -1,10 +1,10 @@
-import logo from "@/assets/sca-ui-logo.png.asset.json";
+import logo from "@/assets/SCA UI Logo1.png";
 import { cn } from "@/lib/utils";
 
 export function BrandMark({ className }: { className?: string }) {
   return (
     <img
-      src={logo.url}
+      src={logo}
       alt="She Code Africa UI Chapter logo"
       className={cn("h-10 w-10 rounded-full object-contain", className)}
     />
