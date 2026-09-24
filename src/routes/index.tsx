7700693632c39c934ed5816e159e212ab2654b5d
Oyinade003 <opedamola003@gmail.com>
2026-09-24 07:45:45 +0100

    Updated member count logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -133,9 +133,12 @@ function Index() {
 
             <dl className="mt-12 grid max-w-md grid-cols-3 gap-6">
               {[
-                { label: "Members", value: stats?.members ?? 69 },
+                { label: "Members", value: 70 },
                 { label: "Learning tracks", value: 13 },
-                { label: "Open opportunities", value: stats?.opportunities ?? 5 },
+                { label: "Open opportunities", value: 5 },
+                // { label: "Members", value: stats?.members ?? 0 },
+                // { label: "Learning tracks", value: 13 },
+                // { label: "Open opportunities", value: stats?.opportunities ?? 0 },
               ].map((s) => (
                 <div key={s.label}>
                   <dt className="sr-only">{s.label}</dt>
